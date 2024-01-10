6a93fcf2d8af6749152186e2d6925d57c9df812b
unknown <gwi01304@naver.com>
2024-01-10 17:35:21 +0900

    달력 선택 오류 처리

--- src/utils/dateUtils.tsx
+++ src/utils/dateUtils.tsx
@@ -9,3 +9,16 @@ export const getDateFormat01 = (getDate: GET_DATE_FORMAT_01) => {
   return `${year}-${month}-${day}`
 }
 type GET_DATE_FORMAT_01 = string | Date
+
+/**
+ * @desc YYYY-MM 형태의 문자열로 변환
+ * @parameter date : Date
+ * @return YYYY-MM
+ */
+export const parseYearAndMonth = (date: Date | String) => {
+  const year = new Date(date.toString()).getFullYear() 
+  let month = (new Date(date.toString()).getMonth() + 1).toString()
+  month = month.length === 1 ? `0${month}` : month
+
+  return `${year}-${month}`
+}
